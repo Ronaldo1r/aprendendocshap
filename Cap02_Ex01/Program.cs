b1c085603d41f93bd116312eff50cab02b8c7fd2
Ronaldo <167239066+Ronaldo1r@users.noreply.github.com>
2024-08-13 16:33:16 -0300

    upload exercícios cap02

---
+++ Cap02_Ex01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap02_Ex01.PNG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string NOME;                                                      /* string atribui um nome */                              
+
+            Console.Write("Entre seu nome: ");
+            NOME = Console.ReadLine();
+            Console.WriteLine("Ola, " + NOME);                                /* o nome recebe ola */
+
+            Console.WriteLine();
+            Console.Write("Tecla <Enter> para encerrar... ");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Cap02_Ex02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap02_Ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, R;
+
+            Console.Write("Entre o 1o. valor: ");  
+            A = int.Parse(Console.ReadLine());            /*numero inteiro soma do A + B */ 
+
+            Console.Write("Entre o 1o. valor: ");
+            B = int.Parse(Console.ReadLine());
+
+            R = A + B;
+
+            Console.WriteLine();
+            Console.WriteLine("Resultado = " + R);
+
+            Console.WriteLine();
+            Console.Write("Tecla <Enter> para encerrar... ");
+            Console.ReadLine();
+
+
+        }
+    }
+}
